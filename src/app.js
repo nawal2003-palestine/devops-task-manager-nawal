d733a791d7531da77c397f597e4bdb90026b0788
nawal2003-palestine <fouzianawal6@gmail.com>
2026-02-18 20:04:32 +0000

    Update welcome message for Lab 1

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ let tasks = [
 ];
 
 app.get('/', (req, res) => {
-  res.json({ message: "DevOps Task Manager API is running (Lab 1)" });
+  res.json({ message: "DevOps Task Manager API is running" });
 });
 
 app.get('/tasks', (req, res) => {
